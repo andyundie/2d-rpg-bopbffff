81b047dc3b47f8ff5cd2251c12ac5e87383c17ee
andyundie <103543220+andyundie@users.noreply.github.com>
2022-09-20 15:00:52 +1000

    Andy 09-20-22

--- BopbfffRPG_Ch5_5_9-5_11/Assets/Scripts/MonoBehaviours/Character.cs
+++ BopbfffRPG_Ch5_5_9-5_11/Assets/Scripts/MonoBehaviours/Character.cs
@@ -6,10 +6,25 @@ public abstract class Character : MonoBehaviour
     public float startingHitPoints;
     public float maxHitPoints;
 
+    public enum CharacterCategory
+    {
+        PLAYER,
+        ENEMY
+    }
+
+    public CharacterCategory characterCatergory;
+
     public virtual void KillCharacter()
     {
         Destroy(gameObject);
     }
     public abstract void ResetCharacter();
     public abstract IEnumerator DamageCharacter(int damage, float interval);
+
+    public virtual IEnumerator FlickerCharacter()
+    {
+        GetComponent<SpriteRenderer>().color = Color.grey;
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
 }
